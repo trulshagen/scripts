c9c46f5e72ef95ac974be1a763204b1683c5ef06
Truls Hagen <truls.hagen@gmail.com>
2012-03-21 12:52:08 +0100

    post-install script

---
+++ post-install.sh
@@ -0,0 +1,28 @@
+sudo apt-get update
+
+packages="build-essential
+manpages-dev
+php5-cli
+python-scapy
+subversion
+git-core
+network-manager-vpnc
+curl
+ubuntu-restricted-extras
+vlc
+mplayer
+gnome-tweak-tool"
+
+sudo apt-get install ${packages}
+
+# Restore placement of window buttons
+gconftool-2 --set "/apps/metacity/general/button_layout" \
+    --type string ":minimize,maximize,close"
+# No event sounds
+gconftool-2 --set "/desktop/gnome/sound/event_sounds" \
+    --type boolean "false"
+
+# gnome-terminal: no menubar, no scrollbar
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/scrollbar_position" \
+    --type string "hidden"
+
